929a7afbe11b53747fbe30db2d3dc4c09767dd31
Abdishakur <yareshakra6@gmail.com>
2025-10-04 08:07:53 +0300

    update

--- src/index.js
+++ src/index.js
@@ -8,24 +8,24 @@ require('dotenv').config();
 const app = express();
 connectDB();
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://simad-web-portal.vercel.app"
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://simad-web-portal.vercel.app"
+// ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    const isAllowed = allowedOrigins.some(o => origin && origin.startsWith(o));
-    if (!origin || isAllowed) {
-      callback(null, true);
-    } else {
-      console.error('Blocked by CORS: ' + origin);
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-app.options('*', cors()); // enable pre-flight across-the-board
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     const isAllowed = allowedOrigins.some(o => origin && origin.startsWith(o));
+//     if (!origin || isAllowed) {
+//       callback(null, true);
+//     } else {
+//       console.error('Blocked by CORS: ' + origin);
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true
+// }));
+// app.options('*', cors()); // enable pre-flight across-the-board
 
 
 app.use(bodyParser.json({ limit: '50mb' }));
