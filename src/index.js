c8aeb495730bdf5b9cd604eecc208f781cadaf5e
Abdishakur <yareshakra6@gmail.com>
2025-10-04 08:45:45 +0300

    update

--- src/index.js
+++ src/index.js
@@ -8,24 +8,8 @@ require('dotenv').config();
 const app = express();
 connectDB();
 
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   "https://simad-web-portal.vercel.app"
-// ];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     const isAllowed = allowedOrigins.some(o => origin && origin.startsWith(o));
-//     if (!origin || isAllowed) {
-//       callback(null, true);
-//     } else {
-//       console.error('Blocked by CORS: ' + origin);
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true
-// }));
-// app.options('*', cors()); // enable pre-flight across-the-board
+app.use(cors());             // <-- This enables CORS for all origins
+app.options('*', cors());    // <-- Enable preflight requests for all routes
 
 
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -36,9 +20,6 @@ app.use(process.env.APP_URL_PREFIX || '/api/v1/app', appApiRoutes);
 
 const adminApiRoutes = require('./Admin/routes/index');
 app.use(process.env.ADMIN_URL_PREFIX, adminApiRoutes);
-
-
-
 
 
 app.use(errorHandler); // error handler
